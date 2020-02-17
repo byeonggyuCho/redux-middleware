f8c43ebf05fc6dbb6e2e5d5df6d678c337bfa7dc
CaterJo <kin7729@naver.com>
2020-02-17 17:59:32 +0900

    init

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 여러개의 미들웨어를 적용 할 수 있습니다.
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware( ReduxThunk,logger))
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
